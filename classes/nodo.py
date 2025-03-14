380ffd249670b8572f0471c7ab2e765de87f744b
Elkin Angulo <elkinsamir643@gmail.com>
2025-03-13 21:52:01 -0500

    buscar_objetivos parcialmente implementado

--- classes/nodo.py
+++ classes/nodo.py
@@ -1,4 +1,5 @@
 from queue import Queue
+import copy
 from helpers.process_map import process_map
 
 # Operadores
@@ -26,11 +27,13 @@ class Nodo:
         self.posicion = posicion
         self.costo = costo
         self.objetivos = objetivos
+        self.objetivos_obtenidos = 0
+        self.visitados = set()
         
     
-    def ver_matriz(self):
+    def ver_matriz(self, matriz):
         result = ""
-        for i in self.matriz:
+        for i in matriz:
             for j in i:
                 result += str(j) + " "
             result += "\n"
@@ -40,42 +43,54 @@ class Nodo:
         
         hijos = []
                 
-        arriba = None if self.posicion[0] - 1 <= 0 else self.matriz[self.posicion[0] - 1][self.posicion[1]]
+        arriba = None if self.posicion[0] - 1 < 0 else self.matriz[self.posicion[0] - 1][self.posicion[1]]
         derecha = None if self.posicion[1] + 1 >= len(self.matriz) else  self.matriz[self.posicion[0]][self.posicion[1] + 1]
         abajo = None if self.posicion[0] + 1 >= len(self.matriz) else self.matriz[self.posicion[0] + 1][self.posicion[1]]
-        izquierda = None if self.posicion[1] - 1 <= 0 else self.matriz[self.posicion[0]][self.posicion[1] - 1]
+        izquierda = None if self.posicion[1] - 1 < 0 else self.matriz[self.posicion[0]][self.posicion[1] - 1]
         
         
         if arriba != 1 and arriba != None:
-            hijos.append(Nodo(self.matriz, (self.posicion[0] - 1, self.posicion[1]), self.objetivos, self, 1, self.profundidad + 1))
+            nueva_matriz = copy.deepcopy(self.matriz)
+            hijo = Nodo(nueva_matriz, (self.posicion[0] - 1, self.posicion[1]), self.objetivos, self, 1, self.profundidad + 1)
+            hijo.objetivos_obtenidos = self.objetivos_obtenidos
+            hijo.visitados = self.visitados
+            hijos.append(hijo)
         if derecha != 1 and derecha != None:
-            hijos.append(Nodo(self.matriz, (self.posicion[0], self.posicion[1] + 1), self.objetivos, self, 2, self.profundidad + 1))
+            nueva_matriz = copy.deepcopy(self.matriz)
+            hijo = Nodo(nueva_matriz, (self.posicion[0], self.posicion[1] + 1), self.objetivos, self, 2, self.profundidad + 1)
+            hijo.objetivos_obtenidos = self.objetivos_obtenidos
+            hijo.visitados = self.visitados
+            hijos.append(hijo)
         if abajo != 1 and abajo != None:
-            hijos.append(Nodo(self.matriz, (self.posicion[0] + 1, self.posicion[1]), self.objetivos, self, 3, self.profundidad + 1))
+            nueva_matriz = copy.deepcopy(self.matriz)
+            hijo = Nodo(nueva_matriz, (self.posicion[0] + 1, self.posicion[1]), self.objetivos, self, 3, self.profundidad + 1)
+            hijo.objetivos_obtenidos = self.objetivos_obtenidos
+            hijo.visitados = self.visitados
+            hijos.append(hijo)
         if izquierda != 1 and izquierda != None:
-            hijos.append(Nodo(self.matriz, (self.posicion[0], self.posicion[1] - 1), self.objetivos, self, 0, self.profundidad + 1))
+            nueva_matriz = copy.deepcopy(self.matriz)
+            hijo = Nodo(nueva_matriz, (self.posicion[0], self.posicion[1] - 1), self.objetivos, self, 0, self.profundidad + 1)
+            hijo.objetivos_obtenidos = self.objetivos_obtenidos
+            hijo.visitados = self.visitados
+            hijos.append(hijo)
                 
         return hijos
     
     def buscar_objetivos(self): 
         
-        initial_objetivos = self.objetivos  # Guardar la cantidad inicial de objetivos
         cola = Queue()
         cola.put(self)
-        objetivos_pos = []
-        visitados = set()
-        visitados.add( ( tuple(self.posicion), 0) )  # Inicializar con 0 objetivos recolectados
+        self.visitados.add( ( tuple(self.posicion), 0) )  # Inicializar con 0 objetivos recolectados
 
-        while not cola.empty():
+        while not cola.empty():    
             
             nodo = cola.get()
-
+            
             # Verificar si la posición actual es un objetivo no recolectado
             if nodo.matriz[nodo.posicion[0]][nodo.posicion[1]] == 4:
-                if nodo.posicion not in objetivos_pos:
-                    objetivos_pos.append(nodo.posicion)
-                    print(objetivos_pos)
-                    if len(objetivos_pos) == initial_objetivos:
+                nodo.objetivos_obtenidos += 1
+                nodo.matriz[nodo.posicion[0]][nodo.posicion[1]] = 0
+                if nodo.objetivos_obtenidos == nodo.objetivos:
                         return nodo
 
             # Generar hijos y procesar
@@ -83,11 +98,13 @@ class Nodo:
 
             
             for hijo in hijos:
-                current_k = len(objetivos_pos)  # Objetivos recolectados hasta ahora
-                estado = (tuple(hijo.posicion), current_k)
-                if estado not in visitados:
-                    visitados.add(estado)
-                    cola.put(hijo)
+                estado = (tuple(hijo.posicion), hijo.objetivos_obtenidos)
+                if estado not in nodo.visitados:
+                    hijo.visitados.add(estado)        
+                    cola.put(hijo)       
+            
+            print(nodo.obtener_ruta())  
+            
                 
         return None
             
@@ -95,11 +112,20 @@ class Nodo:
     def obtener_ruta(self):
         ruta = []
         nodo = self
-        while nodo.padre:
+        while nodo.padre != None:
             ruta.append(nodo.posicion)
             nodo = nodo.padre
-        ruta.append(nodo.posicion)
         return ruta[::-1]
+
+    def obtener_ruta_matriz(self, ruta):
+        # Hacer una matriz que muestre con flechas ascii la ruta a seguir
+        matriz = copy.deepcopy(self.matriz)
+        for i in ruta:
+            matriz[i[0]][i[1]] = "x"
+        return matriz
+        
+            
+        
         
         
     def __str__(self):
@@ -130,7 +156,12 @@ root = Nodo(matrix, player_position, objetivos)
 print("Posición inicial del jugador: ", root.posicion)
 print("Cantidad de objetivos: ", root.objetivos)
 objetivos = root.buscar_objetivos()
-print(objetivos.obtener_ruta())
+
+print(objetivos)
+
+# print(objetivos.ver_matriz(objetivos.obtener_ruta_matriz(objetivos.obtener_ruta())))
+    
+    
 
 # print(objetivos)
 
