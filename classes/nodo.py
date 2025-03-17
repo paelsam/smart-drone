7fc135cdb769393b2d2182e0411ddaaf53fb93ba
Elkin Angulo <elkinsamir643@gmail.com>
2025-03-17 17:18:01 -0500

    Animacion del dron añadida

--- classes/nodo.py
+++ classes/nodo.py
@@ -1,6 +1,7 @@
 from queue import Queue
 from helpers.process_map import process_map
 from helpers.determinar_operador import determinar_operador
+from helpers.process_path import process_path
 from colorama import Fore, Style
 
 class Nodo:
@@ -89,35 +90,3 @@ class Nodo:
             
     def __str__(self):
         return  f"Operador: {determinar_operador(self.operador)}\nProfundidad: {self.profundidad}\nObjetivos faltantes: {self.objetivos - len(self.objetivos_posiciones)}\nPosicion: {self.posicion}\nCosto: {self.costo}\n"
-    
-
-
-
-# Procesar matriz de texto 
-
-matrix = process_map("./assets/maps_files/test4.txt")
-player_position = None
-objetivos = 0
-queue = Queue()
-
-# Buscar la posición del jugador (numero 2) y la cantidad de objetivos (numero 4)
-
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        if matrix[i][j] == 2:
-            player_position = [i, j]
-        if matrix[i][j] == 4:
-            objetivos += 1
-
-# Crear nodo raíz
-root: Nodo = Nodo(matrix, player_position, objetivos)
-print("Posición inicial del jugador: ", root.posicion)
-print("Cantidad de objetivos: ", root.objetivos)
-objetivos: Nodo = root.buscar_objetivos()
-
-print(objetivos)
-print(objetivos.obtener_ruta())
-
-
-print(objetivos.ver_matriz(objetivos.obtener_ruta_matriz(objetivos.obtener_ruta())))
-
